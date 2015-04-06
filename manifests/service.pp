3b1ff79c4a7767e0d9cb954eb29ae914debebd90
Teran McKinney <sega01@go-beyond.org>
2015-04-06 23:59:30 +0000

    Continuing to update OS dependencies and support for CentOS/Redhat/Debian 7
    Some cleanups as well.

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,29 +11,52 @@ class teagent::service {
     File['/var/lib/te-agent/config_teagent.sh']
   ]
 
-  # upstart provider doesn't work well on redhat/centos
-  case $::operatingsystem {
+ case $::operatingsystem {
     centos, redhat, ubuntu: {
-      service { 'te-agent':
-        ensure     => running,
-        hasrestart => true,
-        hasstatus  => true,
-        start      => '/sbin/start te-agent',
-        stop       => '/sbin/stop te-agent',
-        restart    => '/sbin/stop te-agent; /sbin/start te-agent',
-        status     => '/sbin/status te-agent',
-        require    => $service_require,
-        subscribe  => File['/var/lib/te-agent/config_teagent.sh'],
+      $customservice = true
+      if ($::osfamily == 'RedHat') {
+        if ($::operatingsystemrelease =~ /^7/) {
+          # systemd
+          $start = '/usr/bin/systemctl start te-agent'
+          $stop = '/usr/bin/systemctl stop te-agent'
+          $restart = '/usr/bin/systemctl restart te-agent'
+          $status = '/usr/bin/systemctl status te-agent'
+        }
+      }
+      else {
+        # Upstart
+        $start = '/sbin/start te-agent'
+        $stop = '/sbin/stop te-agent'
+        $restart = '/bin/restart te-agent'
+        $status = '/bin/status te-agent'
       }
     }
     default: {
-      service { 'te-agent':
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        require    => $service_require,
-      }
+      $customservice = false
+    }
+  }
+
+  if ($customservice == false) {
+    # Use defaults if none explictly set.
+    service { 'te-agent':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+      require    => $service_require,
+    }
+  }
+  else {
+    service { 'te-agent':
+      ensure     => running,
+      hasrestart => true,
+      hasstatus  => true,
+      start      => $start,
+      stop       => $stop,
+      restart    => $restart,
+      status     => $status,
+      require    => $service_require,
+      subscribe  => File['/var/lib/te-agent/config_teagent.sh'],
     }
   }
 }
