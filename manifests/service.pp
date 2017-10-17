5a6cde476a7bde84b0431bb7c1a0deec546dd0c9
Gaston Bezzi <gaston@thousandeyes.com>
2017-10-17 16:09:59 +0100

    CA-314: First commit for puppet 5 support.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,67 +1,32 @@
-# == Class: teagent::service
-# Copyright © 2013 ThousandEyes, Inc.
+# == Class: te_agent::service
+#
+# Handles the te-agent service.
 #
 # === Copyright
 #
-# Copyright © 2013 ThousandEyes, Inc.
+# Copyright © 2017 ThousandEyes, Inc.
 #
-class teagent::service {
-  $service_require = [
-    Package['te-agent'],
-    File['/var/lib/te-agent/config_teagent.sh']
-  ]
+class te_agent::service {
 
- case $::operatingsystem {
-    centos, redhat, ubuntu: {
-      $customservice = true
-      if ($::osfamily == 'RedHat') and ($::operatingsystemrelease =~ /^7/) {
-        # systemd
-        $start = '/usr/bin/systemctl start te-agent'
-        $stop = '/usr/bin/systemctl stop te-agent'
-        $restart = '/usr/bin/systemctl restart te-agent' # This will work even if te-agent is stopped.
-        $status = '/usr/bin/systemctl status te-agent'
-      }
-      elsif $::lsbdistcodename == 'xenial' {
-        # systemd
-        $start = '/bin/systemctl start te-agent'
-        $stop = '/bin/systemctl stop te-agent'
-        $restart = '/bin/systemctl restart te-agent' # This will work even if te-agent is stopped.
-        $status = '/bin/systemctl status te-agent'
-      }
-      else {
-        # Upstart
-        $start = '/sbin/start te-agent'
-        $stop = '/sbin/stop te-agent'
-        $restart = '/sbin/stop te-agent; /sbin/start te-agent' # /bin/restart won't work if it's not running.
-        $status = '/sbin/status te-agent'
-      }
-    }
-    default: {
-      $customservice = false
+  $te_agent_service_ensure = $te_agent::te_agent ? {
+    true  => 'running',
+    false => 'stopped',
+  }
+
+  # Addresses issue to fetch init system on RedHat < 7 versions.
+  if ($facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['full'],'7') < 0 ) {
+    Service {
+      provider => 'upstart',
     }
   }
 
-  if ($customservice == false) {
-    # Use defaults if none explictly set.
-    service { 'te-agent':
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      hasstatus  => true,
-      require    => $service_require,
-    }
+  service { 'te-agent':
+    ensure     => $te_agent_service_ensure,
+    enable     => $te_agent::te_agent,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => [Package['te-agent'],File['/etc/te-agent.cfg']],
+    subscribe  => File['/etc/te-agent.cfg'],
   }
-  else {
-    service { 'te-agent':
-      ensure     => running,
-      hasrestart => true,
-      hasstatus  => true,
-      start      => $start,
-      stop       => $stop,
-      restart    => $restart,
-      status     => $status,
-      require    => $service_require,
-      subscribe  => File['/var/lib/te-agent/config_teagent.sh'],
-    }
-  }
+
 }
